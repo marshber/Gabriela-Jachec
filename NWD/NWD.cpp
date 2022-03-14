51ebc7ce3fe20e89809f862bafef8169371a2cc0
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-03-14 11:05:50 +0100

    Dodano NWW i NWD

---
+++ NWD/NWD.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	cout << "Podaj pierwszą liczbę: ";
+	cin >> a;
+	cout << endl << "Podaj drugą liczbę: ";
+	cin >> b;
+	
+	 while (a != b)
+	 {
+		 if (a < b) b = b - a;
+		 else a = a - b;
+	 }
+	
+	 cout << endl << "Największy wspólny dzielnik :" << a << endl;
+}
---
+++ NWW/NWW.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int NWW;
+	int iloczyn = a * b;
+
+	while (a != b)
+	{
+		if (a < b) b = b - a;
+		else a = a - b;
+	}
+	NWW = iloczyn / a;
+	cout << "Najmniejsza wspólna wielokrotność: " << NWW;
+}
