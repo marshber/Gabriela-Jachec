262963c193557f926f40c772fcdf6ed1c1a593b7
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-03-28 11:03:46 +0200

    fajny plik

---
+++ szyfr cezara/szyfr cezara.cpp
@@ -0,0 +1,26 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	char tab[1001];
+	int klucz;
+
+	cout << "Podaj wyraz składający się z małych liter:";
+	cin >> tab;
+
+	cout << "Podaj klucz z przedziału [-26...26]: ";
+	cin >> klucz;
+
+	int dl = rozmiar(tab);
+
+	if (klucz >= 26 && k
+
+
+
+	cout << "Po zaszyfrowaniu: " << tab << endl;
+
+	cout << "Po rozszyfrowaniu: " << tab << endl;
+
+	return 0;
+}
