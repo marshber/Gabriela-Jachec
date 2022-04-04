342bf9b4e327b387108508bdf4f6298752822214
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-04-04 11:06:19 +0200

    zaaktualizowano szyfr cezara

--- szyfr cezara/szyfr cezara.cpp
+++ szyfr cezara/szyfr cezara.cpp
@@ -1,10 +1,13 @@
 ﻿using namespace std;
 #include <iostream>
 
+void Szyfr(int klucz, char tab[]);
+
 int main()
 {
 	char tab[1001];
 	int klucz;
+
 
 	cout << "Podaj wyraz składający się z małych liter:";
 	cin >> tab;
@@ -12,15 +15,33 @@ int main()
 	cout << "Podaj klucz z przedziału [-26...26]: ";
 	cin >> klucz;
 
-	int dl = rozmiar(tab);
-
-	if (klucz >= 26 && k
-
-
+	Szyfr;
 
 	cout << "Po zaszyfrowaniu: " << tab << endl;
 
 	cout << "Po rozszyfrowaniu: " << tab << endl;
 
 	return 0;
+}
+void Szyfr(int klucz, char tab[]) {
+
+	int dl = strlen(tab);
+
+	if (!(klucz >= -26 && klucz <= 26))
+
+		if (klucz >= 0)
+			for (char i = 0; i < dl; i++) 
+			{
+				if (tab[i] + klucz <= 'z') tab[i] += klucz;
+				else tab[i] = tab[i] + klucz - 26;
+			}
+		else
+			for (char i; i < dl;i++) 
+			{
+			if (tab[i] + klucz >= 'a') tab[i] += klucz;
+		else tab[i] = tab[i] + klucz + 26;
+		}
+	 
+	
+
 }
