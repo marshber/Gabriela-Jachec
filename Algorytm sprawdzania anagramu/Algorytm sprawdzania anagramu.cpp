38348e345ee2ef037774ce47f5d7107718ad4802
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-04-25 11:04:15 +0200

    dasopdfasjdf

---
+++ Algorytm sprawdzania anagramu/Algorytm sprawdzania anagramu.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+bool Funkcja(char wyraz1, char wyraz2)
+
+int main()
+{
+	char wyraz1;
+	char wyraz2;
+	char licz[123];
+
+	cout << "Podaj pierwszy wyraz: ";
+	cin >> wyraz1;
+	cout << "Podaj drugi wyraz: ";
+	cin >> wyraz2;
+
+
+
+
+}
+
+bool Funkcja(char wyraz1, char wyraz2) {
+
+	char licz[123];
+
+	int dl1 = strlen(wyraz1);
+	int dl2 = strlen(wyraz2);
+
+	if (dl1 != dl2) cout << false;
+	licz[] = {}
+		for(char i = 0; i < dl1; i++)
+}
