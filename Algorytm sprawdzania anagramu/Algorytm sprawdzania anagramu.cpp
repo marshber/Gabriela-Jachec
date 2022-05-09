c41a80fe98c5ed7a60e9e800725f75ed6ac45162
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-05-09 10:46:19 +0200

    uwu

--- Algorytm sprawdzania anagramu/Algorytm sprawdzania anagramu.cpp
+++ Algorytm sprawdzania anagramu/Algorytm sprawdzania anagramu.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 
-bool Funkcja(char wyraz1, char wyraz2)
+bool Funkcja(char *wyraz1, char *wyraz2);
 
 int main()
 {
-	char wyraz1;
-	char wyraz2;
+	char wyraz1[123];
+	char wyraz2[123];
 	char licz[123];
 
 	cout << "Podaj pierwszy wyraz: ";
@@ -14,19 +14,25 @@ int main()
 	cout << "Podaj drugi wyraz: ";
 	cin >> wyraz2;
 
-
-
-
+	if (Funkcja(wyraz1, wyraz2) == true) cout << "Podane wyrazy są anagramami";
+	else cout << "Podane wyrazy nie są anagranami";
+		
 }
 
-bool Funkcja(char wyraz1, char wyraz2) {
-
-	char licz[123];
+bool Funkcja(char *wyraz1, char *wyraz2) {
 
 	int dl1 = strlen(wyraz1);
 	int dl2 = strlen(wyraz2);
 
-	if (dl1 != dl2) cout << false;
-	licz[] = {}
-		for(char i = 0; i < dl1; i++)
+	if (dl1 != dl2) return false;
+	
+	char licz[123] = {};
+	for (int i = 0; i < dl1; i++)
+		licz[wyraz1[i]]++;
+	for (int i = 0; i < dl1; i++)
+		licz[wyraz2[i]]--;
+
+		for (int i = 0; i < 123; i++)
+			if (licz[i] != 0) return false;
+		return true;
 }
