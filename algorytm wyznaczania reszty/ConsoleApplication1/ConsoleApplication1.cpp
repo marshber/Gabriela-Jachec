27dded7ef4a2bcc8fa2d900a645d618d77cf63cd
Gabriela Jacheć (Uczeń) <gjachec450@technikumkreatywne.pl>
2022-05-16 10:45:01 +0200

    hehe hoohoo weeewooo peeeeeew

---
+++ algorytm wyznaczania reszty/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nominaly[] = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+	int i = 0;
+
+	int kwota;
+	int ile;
+	cout << "Podaj resztę do wpłacenia: ";	
+	cin >> kwota;
+
+	while (kwota > 0)
+	{
+		if (kwota >= nominaly[i]) {
+			ile = kwota / nominaly[i];
+			kwota = kwota - nominaly[i] * ile;
+			cout << nominaly[i] << " x " << ile << endl;
+			
+		}
+		i = i + 1;
+	}
+}
